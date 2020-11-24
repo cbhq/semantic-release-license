40c7ed1d50939c8f90fe0a4a31a742b6952ed692
Geoffroy Empain <geoffroy@charlie-bravo.be>
2020-11-24 13:56:07 +0100

    fix: bad semver regex

--- src/handlers/bsl.spec.ts
+++ src/handlers/bsl.spec.ts
@@ -12,9 +12,10 @@ describe('bsl', () => {
   afterEach(() => jest.restoreAllMocks());
 
   it('should update license', async () => {
-    const updatedLicense = await bsl(license, <any>{ nextRelease: { version: '1.1.0' } });
+    const updatedLicense = await bsl(license, <any>{ nextRelease: { version: '1.1.0-beta.2' } });
     const date = new Date();
-    expect(updatedLicense.indexOf('MyApp 1.1.0') !== -1).toBeTruthy();
+    console.log(updatedLicense);
+    expect(updatedLicense.indexOf('MyApp 1.1.0-beta.2') !== -1).toBeTruthy();
     expect(updatedLicense.indexOf(`Change Date:          ${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`) !== -1).toBeTruthy();
     expect(updatedLicense.indexOf(`(c) ${date.getFullYear()} My Company`) !== -1).toBeTruthy();
   });
--- src/handlers/bsl.ts
+++ src/handlers/bsl.ts
@@ -2,7 +2,7 @@ import { Context } from 'semantic-release';
 
 export function bsl(content: string, context: Context): string {
   return content
-    .replace(/(Licensed Work.*)(\d+.\d+.\d+)/, (full, part1) => (
+    .replace(/(Licensed Work:.*)(\d\.\d\.\d.*)/, (full, part1) => (
       `${part1}${context.nextRelease.version}`
     ))
     .replace(/(Change Date.*)(\d{4}-\d{2}-\d{2})/, (full, part1) => {
